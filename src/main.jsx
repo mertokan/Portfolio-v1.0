9fa4fb89d6ffeb46e983be7a8c888dd856c950b5
Mert <mertokan5020@gmail.com>
2023-08-14 12:48:28 +0300

    @vercel/analytics eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom'
+import {Analytics} from '@vercel/analytics/react'
 import routes from './routes'
 import './assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={routes} />
+    <Analytics />
   </React.StrictMode>
 )
