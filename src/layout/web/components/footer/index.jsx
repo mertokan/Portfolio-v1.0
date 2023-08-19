1101d9a6b4fc8994736aa1c532da3dcd0f17a462
Mert <mertokan5020@gmail.com>
2023-08-19 11:30:19 +0300

    footer tasarım hatası düzeltildi

--- src/layout/web/components/footer/index.jsx
+++ src/layout/web/components/footer/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-[60px] flex justify-center items-center bg-site-darkcolor1 text-white container mx-auto'>
-      <div className='text-center'>
+    <div className='h-[60px] flex justify-center items-center bg-site-darkcolor1 text-white '>
+      <div className='text-center container mx-auto'>
         © 2023 Designed by <b>Laralink</b>. Coded by <b>Mert OKAN</b>. All right reserved.
       </div>
     </div>
